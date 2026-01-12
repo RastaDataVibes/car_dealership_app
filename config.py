22fa58bcadd5faacea801f0a68b7cb826d059efd
Opio Bethle <opiobethle@gmail.com>
2026-01-12 21:10:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://zaga_admin:Y1hfxNhumANyST9BcyjxO0RC4m2KXOA2@dpg-d3n1av49c44c73cgjts0-a.oregon-postgres.render.com:5432/used_car_dealership_db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://bisle_admin:C0pMvogaeVEDOrEWML5NdEBjVokXCHC8@dpg-d5ia7n94tr6s73a2loog-a.oregon-postgres.render.com/gc_db_ei5a"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "rasta_secret_420"
 
